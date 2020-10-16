fe18a999c60e22288ec4b1e5df3ac9aa2f59d63b
Tim van Dijkhuizen <timvandijkhuizen@hotmail.nl>
2020-10-16 17:24:11 +0200

    Added ExceptionHelper

---
+++ src/nl/timvandijkhuizen/spigotutils/helpers/ExceptionHelper.java
@@ -0,0 +1,19 @@
+package nl.timvandijkhuizen.spigotutils.helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExceptionHelper {
+
+    public static String[] getStackTrace(Throwable cause) {
+        List<String> lines = new ArrayList<String>();
+        
+        lines.add("[" + cause.getClass().getSimpleName() + "] Cause [" + cause.getMessage() + "]");
+        for(StackTraceElement ste : cause.getStackTrace()) {
+            lines.add(ste.toString());
+        }
+        
+        return lines.toArray(new String[lines.size()]);
+    }
+    
+}
