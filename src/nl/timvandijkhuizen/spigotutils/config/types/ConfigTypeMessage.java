e092b3912d33487b0494a4b40704c7ae7d1d4306
Tim van Dijkhuizen <timvandijkhuizen@hotmail.nl>
2020-10-19 23:24:47 +0200

    ConfigTypeMessage NPE bugfix

--- src/nl/timvandijkhuizen/spigotutils/config/types/ConfigTypeMessage.java
+++ src/nl/timvandijkhuizen/spigotutils/config/types/ConfigTypeMessage.java
@@ -10,6 +10,11 @@ public class ConfigTypeMessage extends ConfigTypeString {
     @Override
     public String getValue(OptionConfig config, ConfigOption<String> option) {
     	String raw = super.getValue(config, option);
+    	
+    	if(raw == null) {
+    		return null;
+    	}
+    	
     	return ChatColor.translateAlternateColorCodes('&', raw);
     }
 	
