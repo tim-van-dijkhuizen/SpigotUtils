1d3415ea0bb708426a0c04ef1c6826982f0ac51e
Tim van Dijkhuizen <timvandijkhuizen@hotmail.nl>
2020-12-17 23:57:14 +0100

    This method is now optional

--- src/nl/timvandijkhuizen/spigotutils/SpigotUtils.java
+++ src/nl/timvandijkhuizen/spigotutils/SpigotUtils.java
@@ -1,7 +1,5 @@
 package nl.timvandijkhuizen.spigotutils;
 
-import nl.timvandijkhuizen.spigotutils.services.Service;
-
 public class SpigotUtils extends PluginBase {
 
     private static SpigotUtils instance;
@@ -9,11 +7,6 @@ public class SpigotUtils extends PluginBase {
     @Override
     public void init() throws Throwable {
         instance = this;
-    }
-    
-    @Override
-    public Service[] registerServices() throws Throwable {
-        return new Service[] {};
     }
     
     public static SpigotUtils getInstance() {
